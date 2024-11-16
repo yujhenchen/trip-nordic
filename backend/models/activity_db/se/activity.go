b8f37d2a943a8c37a87be7ef0e3c04701d47d081
yujhenchen <yujhen.chen93@gmail.com>
2024-11-16 16:11:03 +0800

    fix: data type of mongo db category

--- backend/models/activity_db/se/activity.go
+++ backend/models/activity_db/se/activity.go
@@ -1,12 +1,12 @@
 package activity_db
 
-import "encoding/json"
+import activity_api "backend/models/activity_api/se"
 
 type Result struct {
-	ID            int             `json:"id"`
-	Title         string          `json:"title"`
-	Href          string          `json:"href"`
-	Text          string          `json:"text"`
-	OriginalTitle string          `json:"original_title"` // NOTE: alternative if Title does not exist
-	Categories    json.RawMessage `json:"categories"`     // FIX ME: this is either []string or string
+	ID            int                     `json:"id"`
+	Title         string                  `json:"title"`
+	Href          string                  `json:"href"`
+	Text          string                  `json:"text"`
+	OriginalTitle string                  `json:"original_title"` // NOTE: alternative if Title does not exist
+	Categories    activity_api.Categories `json:"categories"`     // FIX ME: this is either []string or string
 }
