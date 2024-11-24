be7f03d13d7482c1b7f7aab25e0c83a41f5e9bae
yujhenchen <yujhen.chen93@gmail.com>
2024-11-24 13:43:26 +0800

    feat: correct encode using gob encoder

--- backend/utils/hash.go
+++ backend/utils/hash.go
@@ -1,21 +1,34 @@
 package utils
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"encoding/gob"
 	"encoding/hex"
-	"encoding/json"
-	"fmt"
 )
 
-func GenerateHash(obj interface{}) (string, error) {
-	jsonData, err := json.Marshal(obj)
+func getEncode(obj interface{}) (bytes.Buffer, error) {
+	// Initialize the encoder and decoder. Normally enc and dec would be
+	// bound to buf and the encoder and decoder would
+	// run in different processes.
+	// TODO: what is bytes.Buffer how does it work
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(obj)
 	if err != nil {
-		fmt.Printf("Error marshaling object: %v", err)
+		// log.Fatal("encode error:", err)
+		return bytes.Buffer{}, err
+	}
+	return buf, nil
+}
+
+func GenerateHash(obj interface{}) (string, error) {
+	buf, err := getEncode(obj)
+	if err != nil {
 		return "", err
 	}
-
 	hash := sha256.New()
-	hash.Write(jsonData)
+	hash.Write(buf.Bytes())
 	finalHash := hex.EncodeToString(hash.Sum(nil))
 	return finalHash, nil
 }
