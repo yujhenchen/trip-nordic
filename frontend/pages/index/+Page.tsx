7bc3fa5f678812ffdd6a30dd44c06dfcdc2e600e
yujhenchen <yujhen.chen93@gmail.com>
2024-12-31 18:42:52 +0800

    feat: use TypographyH1

--- frontend/pages/index/+Page.tsx
+++ frontend/pages/index/+Page.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Counter } from "./Counter.jsx";
+import { TypographyH1 } from "@/components/typography/H1.jsx";
 
 export default function Page() {
   return (
     <>
-      <h1 className={"font-bold text-3xl pb-4"}>My Vike app</h1>
-      This page is:
+      <TypographyH1 text="Plan Your Next Nordic Adventure" />
+      {/* This page is:
       <ul>
         <li>Rendered to HTML.</li>
         <li>
           Interactive. <Counter />
         </li>
-      </ul>
+      </ul> */}
     </>
   );
 }
