4e6b19bb9669a80fde8b262cd19171630015253e
yujhenchen <yujhen.chen93@gmail.com>
2024-12-31 18:07:34 +0800

    feat: footer

---
+++ frontend/components/Footer.tsx
@@ -0,0 +1,34 @@
+import { Link } from "./Link";
+
+export default function Footer() {
+  return (
+    <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2025{" "}
+          <a
+            href="https://github.com/yujhenchen/trip-nordic"
+            className="hover:underline"
+          >
+            TripNordic™
+          </a>
+          . All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/login">Login</Link>
+          </li>
+          <li>
+            <Link href="/plan">Plan</Link>
+          </li>
+          <li>
+            <Link href="/explore">Explore</Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
