bf274085ad22105458226349630438c5cea47500
yujhenchen <yujhen.chen93@gmail.com>
2025-01-06 00:35:05 +0800

    feat: clean up

--- frontend/components/common/PageContent.tsx
+++ frontend/components/common/PageContent.tsx
@@ -1,18 +1,9 @@
-import clsx from "clsx";
 import { ReactNode } from "react";
 
 export default function PageContent({ children }: { children: ReactNode }) {
   return (
-    <main id="page-container">
-      <div
-        id="page-content"
-        className={clsx(
-          "p-5 pb-12"
-          // , "min-h-screen"
-        )}
-      >
-        {children}
-      </div>
+    <main id="page-container" className="p-5 pb-12 min-h-screen">
+      {children}
     </main>
   );
 }
--- frontend/pages/explore/+Page.tsx
+++ frontend/pages/explore/+Page.tsx
@@ -22,7 +22,7 @@ function FilterWrapper() {
 
 function CardWrapper() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {Array.from({ length: 15 }, (_, i) => (
         <Card>
           <CardHeader>
