e5203717b59a067063811e1c49fde85e38d361a1
yujhenchen <yujhen.chen93@gmail.com>
2025-01-05 21:04:30 +0800

    feat: place footer bottom, remove min height of page container

--- frontend/components/common/Footer.tsx
+++ frontend/components/common/Footer.tsx
@@ -2,7 +2,7 @@ import { Link } from "./Link";
 
 export default function Footer() {
   return (
-    <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800 absolute bottom-0 w-full">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2025{" "}
--- frontend/components/common/PageContent.tsx
+++ frontend/components/common/PageContent.tsx
@@ -1,11 +1,18 @@
+import clsx from "clsx";
 import { ReactNode } from "react";
 
 export default function PageContent({ children }: { children: ReactNode }) {
   return (
-    <div id="page-container">
-      <div id="page-content" className={"p-5 pb-12 min-h-screen"}>
+    <main id="page-container">
+      <div
+        id="page-content"
+        className={clsx(
+          "p-5 pb-12"
+          // , "min-h-screen"
+        )}
+      >
         {children}
       </div>
-    </div>
+    </main>
   );
 }
