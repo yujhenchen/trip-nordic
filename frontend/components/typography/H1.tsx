67d23b83984d1ff7485d0df8a58e46d21bf28ca7
yujhenchen <yujhen.chen93@gmail.com>
2025-01-05 21:05:05 +0800

    feat: classname as prop

--- frontend/components/typography/H1.tsx
+++ frontend/components/typography/H1.tsx
@@ -1,6 +1,19 @@
-export function TypographyH1({ text }: { text: string }) {
+import clsx from "clsx";
+
+export function TypographyH1({
+  text,
+  className,
+}: {
+  text: string;
+  className?: string;
+}) {
   return (
-    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    <h1
+      className={clsx(
+        "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
+        className
+      )}
+    >
       {text}
     </h1>
   );
