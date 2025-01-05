462af79b15c416bbd5ca96b27a126d7343cc37e1
yujhenchen <yujhen.chen93@gmail.com>
2025-01-05 23:32:16 +0800

    feat: use card

--- frontend/pages/explore/+Page.tsx
+++ frontend/pages/explore/+Page.tsx
@@ -1,3 +1,43 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Page() {
-  return <div>Explore Page</div>;
+  return (
+    <div>
+      <FilterWrapper />
+      <CardWrapper />
+    </div>
+  );
+}
+
+function FilterWrapper() {
+  return <div>Filter Wrapper</div>;
+}
+
+function CardWrapper() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+      {Array.from({ length: 15 }, (_, i) => (
+        <Card>
+          <CardHeader>
+            <img src="https://via.placeholder.com/150x100" alt="Card Image" />
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          {/* <CardContent>
+            <p>Card Content</p>
+          </CardContent> */}
+          {/* <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter> */}
+        </Card>
+      ))}
+    </div>
+  );
 }
