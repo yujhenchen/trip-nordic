6e33070552e6f731b20fb8f12706681644d71c3d
yujhenchen <yujhen.chen93@gmail.com>
2025-01-06 00:34:35 +0800

    feat: header nav menu

--- frontend/components/common/Header.tsx
+++ frontend/components/common/Header.tsx
@@ -1,19 +1,59 @@
 import Logo from "./Logo";
-import { Link } from "./Link";
 import Sidebar from "./Sidebar";
 import { ModeToggle } from "./modeToggle";
+import {
+  Link,
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@radix-ui/react-navigation-menu";
+import { navigationMenuTriggerStyle } from "../ui/navigation-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
 export default function Header() {
   return (
-    <header>
-      <Sidebar>
-        <Logo />
-        <Link href="/">Home</Link>
-        <Link href="/login">Login</Link>
-        <Link href="/plan">Plan</Link>
-        <Link href="/explore">Explore</Link>
-        <ModeToggle />
-      </Sidebar>
+    <header className="flex py-8 items-center w-full">
+      <Logo />
+      <NavigationMenu className="w-full px-6">
+        <NavigationMenuList className="flex items-center">
+          <div className="flex grow">
+            <NavigationMenuItem>
+              <Link href="/plan">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Plan
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+
+            <NavigationMenuItem>
+              <Link href="/explore">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Explore
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </div>
+
+          <div className="flex gap-6 items-center">
+            <NavigationMenuItem>
+              <Link href="/login">
+                <Avatar>
+                  <AvatarImage
+                    src="https://via.placeholder.com/150"
+                    className="rounded-full w-12 aspect-square"
+                  />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </Link>
+            </NavigationMenuItem>
+
+            <NavigationMenuItem>
+              <ModeToggle />
+            </NavigationMenuItem>
+          </div>
+        </NavigationMenuList>
+      </NavigationMenu>
     </header>
   );
 }
