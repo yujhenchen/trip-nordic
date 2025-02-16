e92da7eff256786bc8737cd137b1ca5b2e5025a6
yujhenchen <yujhen.chen93@gmail.com>
2025-02-16 14:18:10 +0800

    feat: footer

---
+++ web/app/components/common/CopyRight.tsx
@@ -0,0 +1,14 @@
+export function CopyRight() {
+	return (
+		<span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+			© 2025{" "}
+			<a
+				href="https://github.com/yujhenchen/trip-nordic"
+				className="hover:underline"
+			>
+				TripNordic™
+			</a>
+			. All Rights Reserved.
+		</span>
+	);
+}
--- web/app/components/common/Footer.tsx
+++ web/app/components/common/Footer.tsx
@@ -1,35 +1,59 @@
-import Link from "next/link";
+import {
+	NavigationMenu,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+} from "@radix-ui/react-navigation-menu";
+import { navigationMenuTriggerStyle } from "../ui/navigation-menu";
+import { CopyRight } from "./CopyRight";
 
 export function Footer() {
 	return (
 		<footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800 w-full">
 			<div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-				<span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-					© 2025{" "}
-					<a
-						href="https://github.com/yujhenchen/trip-nordic"
-						className="hover:underline"
-					>
-						TripNordic™
-					</a>
-					. All Rights Reserved.
-				</span>
-				<ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-					<li>
-						<Link href="/" className="hover:underline me-4 md:me-6">
-							Home
-						</Link>
-					</li>
-					<li>
-						<Link href="/login">Login</Link>
-					</li>
-					<li>
-						<Link href="/plan">Plan</Link>
-					</li>
-					<li>
-						<Link href="/explore">Explore</Link>
-					</li>
-				</ul>
+				<CopyRight />
+
+				<NavigationMenu>
+					<NavigationMenuList className="flex items-center">
+						<div className="flex grow">
+							<NavigationMenuItem>
+								<NavigationMenuLink
+									className={navigationMenuTriggerStyle()}
+									href="/"
+								>
+									Home
+								</NavigationMenuLink>
+							</NavigationMenuItem>
+
+							<NavigationMenuItem>
+								<NavigationMenuLink
+									className={navigationMenuTriggerStyle()}
+									href="/plan"
+								>
+									Plan
+								</NavigationMenuLink>
+							</NavigationMenuItem>
+
+							<NavigationMenuItem>
+								<NavigationMenuLink
+									className={navigationMenuTriggerStyle()}
+									href="/explore"
+								>
+									Explore
+								</NavigationMenuLink>
+							</NavigationMenuItem>
+
+							<NavigationMenuItem>
+								<NavigationMenuLink
+									className={navigationMenuTriggerStyle()}
+									href="/about"
+								>
+									About
+								</NavigationMenuLink>
+							</NavigationMenuItem>
+						</div>
+					</NavigationMenuList>
+				</NavigationMenu>
 			</div>
 		</footer>
 	);
