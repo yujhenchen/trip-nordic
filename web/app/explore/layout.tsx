1d2504cc40b1bf8e698b11344b15d5ea5b6f815d
yujhenchen <yujhen.chen93@gmail.com>
2025-02-16 12:12:33 +0800

    feat: explore page layout

---
+++ web/app/explore/layout.tsx
@@ -0,0 +1,13 @@
+import { DialogProvider } from "../components/providers/DialogProvider";
+
+export default function ExploreLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <DialogProvider>
+      <section>{children}</section>
+    </DialogProvider>
+  );
+}
