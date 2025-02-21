de6853109feed190e39b64a6d93376a5979c740c
yujhenchen <yujhen.chen93@gmail.com>
2025-02-21 19:26:06 +0800

    feat: popup toast on click

--- web/app/plan/ControlPanel.tsx
+++ web/app/plan/ControlPanel.tsx
@@ -1,11 +1,30 @@
+"use client";
+
 import { ClipboardCopy, Save } from "lucide-react";
 import { IconButton } from "@/components/common/IconButton";
+import { useToast } from "@/hooks/use-toast";
 
 export function ControlPanel() {
+	const { toast } = useToast();
+
 	return (
 		<div className="w-full h-16 place-content-end flex space-x-4 border px-8 items-center">
-			<IconButton icon={<Save />} />
-			<IconButton icon={<ClipboardCopy />} />
+			<IconButton
+				icon={<Save />}
+				onClick={() =>
+					toast({
+						title: "Saved",
+					})
+				}
+			/>
+			<IconButton
+				icon={<ClipboardCopy />}
+				onClick={() =>
+					toast({
+						title: "Copied",
+					})
+				}
+			/>
 		</div>
 	);
 }
