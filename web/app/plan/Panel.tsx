c2bded1f9df02de7abea17cf5acd19165706eabd
yujhenchen <yujhen.chen93@gmail.com>
2025-02-21 17:12:34 +0800

    fix: lint

--- web/app/plan/Panel.tsx
+++ web/app/plan/Panel.tsx
@@ -47,7 +47,7 @@ function PanelContainer({
 		<Card
 			className={cn(
 				"bg-gray-200 overflow-hidden shrink-0 w-full h-full md:w-72 flex flex-col",
-				className
+				className,
 			)}
 			{...rest}
 		>
