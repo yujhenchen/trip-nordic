00364f19d5e65c07971df2d4b2fc020d7c72624f
yujhenchen <yujhen.chen93@gmail.com>
2025-03-07 18:12:45 +0800

    feat: django rest api with post req

---
+++ backend/restful-api/users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from utils.models_abstracts import AbstractModel
+from django_extensions.db.models import (TimeStampedModel, ActivatorModel, TitleDescriptionModel)
+
+class Contact(TimeStampedModel, ActivatorModel, TitleDescriptionModel, AbstractModel): 
+	class Meta:
+		verbose_name_plural = 'Contacts'
+
+	name = models.CharField(verbose_name='Name', max_length=255)
+	email = models.EmailField(verbose_name='Email', unique=True)
+	message = models.TextField(verbose_name='Message')
+
+	def __str__(self):
+		return f'{self.title}'
---
+++ backend/restful-api/users/utils/models_abstracts.py
@@ -0,0 +1,9 @@
+import uuid
+from django.db import models
+
+class AbstractModel(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+
+	class Meta:
+		abstract = True
+		
