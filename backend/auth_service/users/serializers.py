26f0f7501925f70114265af9da3400eeb3116210
yujhenchen <yujhen.chen93@gmail.com>
2025-03-08 20:35:01 +0800

    feat: remove not need extract pwd

--- backend/auth_service/users/serializers.py
+++ backend/auth_service/users/serializers.py
@@ -4,10 +4,8 @@ from.models import User
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
-		fields = ('email', 'password')
+		fields = ['email', 'password']
 
 	def create(self, validated_data):
-        # Extract password and ensure it's hashed before saving
-		password = validated_data.pop('password')
-		user = User.objects.create_user(**validated_data, password=password)
+		user = User.objects.create_user(**validated_data)
 		return user
