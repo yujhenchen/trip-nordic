56c6abf32ec755048d1ea4dca2444347c213e7c5
yujhenchen <yujhen.chen93@gmail.com>
2025-03-18 21:21:28 +0800

    feat: card dialog title and content edit/ view mode ui switch

--- web/components/dialog/CardDialog.tsx
+++ web/components/dialog/CardDialog.tsx
@@ -7,6 +7,9 @@ import {
 } from "@/components/ui/dialog";
 import { Textarea } from "@/components/ui/textarea";
 import { useState } from "react";
+import { Input } from "../ui/input";
+import { IconButton } from "../common/IconButton";
+import { CheckIcon, CrossIcon, X } from "lucide-react";
 
 interface Props {
 	onClose: () => void;
@@ -14,36 +17,101 @@ interface Props {
 	description: string;
 }
 
-const cardMode = {
+export const IDS = {
+	TITLE_EDIT: "title-edit",
+	CONTENT_EDIT: "content-edit",
+} as const;
+
+const MODE = {
 	EDIT: "EDIT",
 	VIEW: "VIEW",
 } as const;
 
-type CardModeType = keyof typeof cardMode;
+type ModeType = keyof typeof MODE;
+
+const Title = ({ text }: { text: string }) => {
+	const [mode, setMode] = useState<ModeType>(MODE.VIEW);
+	const edit = () => {
+		setMode(MODE.EDIT);
+	};
+
+	const save = () => {
+		setMode(MODE.VIEW);
+	};
+
+	const cancel = () => {
+		setMode(MODE.VIEW);
+	};
+
+	return (
+		<>
+			{mode === MODE.VIEW ? (
+				<DialogTitle onDoubleClick={edit}>{text}</DialogTitle>
+			) : (
+				<div className="flex items-center">
+					<Input
+						id={IDS.TITLE_EDIT}
+						defaultValue={text}
+						onKeyDown={(e) => e.key === "Enter" && save()}
+					/>
+					<IconButton icon={<CheckIcon />} onClick={save} />
+					<IconButton icon={<X />} onClick={cancel} />
+				</div>
+			)}
+		</>
+	);
+};
+
+const Desc = ({ text }: { text: string }) => {
+	const [mode, setMode] = useState<ModeType>(MODE.VIEW);
+
+	const edit = () => {
+		setMode(MODE.EDIT);
+	};
+
+	const save = () => {
+		setMode(MODE.VIEW);
+	};
+
+	const cancel = () => {
+		setMode(MODE.VIEW);
+	};
+
+	return (
+		<>
+			{mode === MODE.VIEW ? (
+				<DialogDescription
+					onDoubleClick={() => {
+						setMode(MODE.EDIT);
+					}}
+				>
+					{text}
+				</DialogDescription>
+			) : (
+				<div className="flex items-center">
+					<Textarea
+						id={IDS.CONTENT_EDIT}
+						defaultValue={text}
+						onKeyDown={(e) => e.key === "Enter" && save()}
+					/>
+					<IconButton icon={<CheckIcon />} onClick={save} />
+					<IconButton icon={<X />} onClick={cancel} />
+				</div>
+			)}
+		</>
+	);
+};
 
 export default function CardDialog({ onClose, title, description }: Props) {
-	const [mode, setMode] = useState<CardModeType>(cardMode.VIEW);
 	return (
 		<Dialog open onOpenChange={onClose}>
 			{/* <DialogTrigger>Open</DialogTrigger> */}
 			<DialogContent className="p-10 w-full md:w-2/3 xl:w-2/5 max-w-full max-h-screen overflow-auto">
 				<div className="flex flex-col md:flex-row space-x-4">
 					<DialogHeader className="py-4 w-3/5">
-						<DialogTitle>{title}</DialogTitle>
-						{mode === cardMode.VIEW ? (
-							<DialogDescription
-								onDoubleClick={() => {
-									setMode(cardMode.EDIT);
-								}}
-							>
-								{description}
-							</DialogDescription>
-						) : (
-							<Textarea
-								defaultValue={description}
-								onKeyDown={(e) => e.key === "Enter" && setMode(cardMode.VIEW)}
-							/>
-						)}
+						<Title text={title} />
+
+						<Desc text={description} />
 					</DialogHeader>
 
 					<div className="flex space-y-4 flex-col w-2/5">
