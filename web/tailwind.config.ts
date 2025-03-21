55d22656ff7e617d729c1ada78f56f875823b0fd
yujhenchen <yujhen.chen93@gmail.com>
2025-03-21 22:13:07 +0800

    feat: add side bar

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -8,8 +8,12 @@ export default {
 		extend: {
 			keyframes: {
 				"caret-blink": {
-					"0%,70%,100%": { opacity: "1" },
-					"20%,50%": { opacity: "0" },
+					"0%,70%,100%": {
+						opacity: "1",
+					},
+					"20%,50%": {
+						opacity: "0",
+					},
 				},
 			},
 			animation: {
@@ -60,6 +64,16 @@ export default {
 					"3": "hsl(var(--chart-3))",
 					"4": "hsl(var(--chart-4))",
 					"5": "hsl(var(--chart-5))",
+				},
+				sidebar: {
+					DEFAULT: "hsl(var(--sidebar-background))",
+					foreground: "hsl(var(--sidebar-foreground))",
+					primary: "hsl(var(--sidebar-primary))",
+					"primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+					accent: "hsl(var(--sidebar-accent))",
+					"accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+					border: "hsl(var(--sidebar-border))",
+					ring: "hsl(var(--sidebar-ring))",
 				},
 			},
 		},
