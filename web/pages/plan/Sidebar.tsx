528e907000e6d51e452e2d979867be277d8ca3f6
yujhenchen <yujhen.chen93@gmail.com>
2025-03-21 16:20:58 +0800

    fix: format

--- web/pages/plan/Sidebar.tsx
+++ web/pages/plan/Sidebar.tsx
@@ -21,7 +21,7 @@ export function Sidebar() {
 				},
 				{
 					"w-full h-6 md:w-6": !sidebarOpen,
-				}
+				},
 			)}
 		>
 			<ToggleButton />
@@ -35,12 +35,12 @@ function ToggleButton() {
 	const [icon, setIcon] = useState<JSX.Element | null>(null);
 	const openIcon = useMemo(
 		() => (isTabletOrBigger ? <PanelRightOpen /> : <PanelTopOpen />),
-		[isTabletOrBigger]
+		[isTabletOrBigger],
 	);
 
 	const closeIcon = useMemo(
 		() => (isTabletOrBigger ? <PanelLeftOpen /> : <PanelBottomOpen />),
-		[isTabletOrBigger]
+		[isTabletOrBigger],
 	);
 
 	useEffect(() => {
