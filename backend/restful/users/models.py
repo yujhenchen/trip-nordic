4eb0c4b0ca790804fb2b0e20891bd30cbaa6e4c1
yujhenchen <yujhen.chen93@gmail.com>
2025-03-21 18:10:24 +0800

    feat: alter user table id type to uuid, allow duplicated

--- backend/restful/users/models.py
+++ backend/restful/users/models.py
@@ -1,10 +1,12 @@
+import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from .managers import UserManager
 
 class User(AbstractUser):
-	username = None
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	username = models.CharField(_("username"), max_length=30, unique=False)
 	email = models.EmailField(_("email address"), unique=True)
 
 	USERNAME_FIELD = "email"
