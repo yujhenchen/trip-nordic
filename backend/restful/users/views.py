f1a9afe3b950d3f7b38ba9e0c15e37a12c5144b5
yujhenchen <yujhen.chen93@gmail.com>
2025-03-29 12:30:43 +0800

    fix: format python file

--- backend/restful/users/views.py
+++ backend/restful/users/views.py
@@ -8,59 +8,80 @@ from rest_framework_simplejwt.exceptions import TokenError
 from django.contrib.auth import authenticate
 from rest_framework.exceptions import AuthenticationFailed
 from django.middleware import csrf
-from utils.token import get_tokens_for_user, set_auth_cookies
+from utils.token import (
+    get_tokens_for_user,
+    set_auth_cookies,
+    delete_auth_cookies,
+)
 from django.conf import settings
+
 
 class SignUpView(views.APIView):
     def post(self, request):
         try:
             user = request.data
-            if User.objects.filter(email=user['email']).exists():
-                return JsonResponse({'error': 'Email already exist'}, status=status.HTTP_400_BAD_REQUEST)
+            if User.objects.filter(email=user["email"]).exists():
+                return JsonResponse(
+                    {"error": "Email already exist"}, status=status.HTTP_400_BAD_REQUEST
+                )
 
-            serializer= UserSerializer(data=user)
+            serializer = UserSerializer(data=user)
             if serializer.is_valid(raise_exception=True):
                 serializer.save()
-                return JsonResponse({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
-            else: 
-                return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                return JsonResponse(
+                    {"message": "User created successfully"},
+                    status=status.HTTP_201_CREATED,
+                )
+            else:
+                return JsonResponse(
+                    serializer.errors, status=status.HTTP_400_BAD_REQUEST
+                )
         except:
-            return JsonResponse({'error': 'Failed to create user'}, status=status.HTTP_400_BAD_REQUEST)
-        
-    def get(self, request):
-        return JsonResponse({'message': 'get signup'}, status=status.HTTP_200_OK)
+            return JsonResponse(
+                {"error": "Failed to create user"}, status=status.HTTP_400_BAD_REQUEST
+            )
 
 
 class LogInView(views.APIView):
     def post(self, request):
         try:
-            email = request.data['email']
-            password = request.data['password']
-        
+            email = request.data["email"]
+            password = request.data["password"]
+
             if not email or not password:
-                return JsonResponse({'error': 'Email and password are required'}, status=status.HTTP_400_BAD_REQUEST)
-            
+                return JsonResponse(
+                    {"error": "Email and password are required"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
             user = authenticate(email=email, password=password)
             if not user:
-                raise AuthenticationFailed('Invalid credentials')
-            
-			# TODO: check active user?
+                raise AuthenticationFailed("Invalid credentials")
+
+            # TODO: check active user?
             tokens = get_tokens_for_user(user)
-            response = JsonResponse( {'user': {'email': user.email}}, status=status.HTTP_200_OK)
-            set_auth_cookies(response, tokens)
+            response = JsonResponse(
+                {"user": {"email": user.email}}, status=status.HTTP_200_OK
+            )
+            set_auth_cookies(response, tokens, user.id)
             # TODO: what is this
             # csrf.get_token(request)
             return response
         except KeyError:
-            return JsonResponse({'error': 'Email and password are required'}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(
+                {"error": "Email and password are required"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         except AuthenticationFailed:
-            return JsonResponse({'error': 'Invalid email or password'}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(
+                {"error": "Invalid email or password"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         except Exception as e:
             print(e)
-            return JsonResponse({'error': 'Unknown error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-		
-    def get(self, request):
-        return JsonResponse({'message': 'get login'}, status=status.HTTP_200_OK)
+            return JsonResponse(
+                {"error": "Unknown error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
 
 class LogOutView(views.APIView):
@@ -68,21 +89,31 @@ class LogOutView(views.APIView):
         try:
             refresh_token = request.COOKIES[settings.REFRESH_TOKEN_COOKIE_NAME]
             if not refresh_token:
-                return JsonResponse({"error": "Refresh token required"}, status=status.HTTP_400_BAD_REQUEST)
-            
-            response = JsonResponse({"message": "Logged out successfully"}, status=status.HTTP_200_OK)
-            
+                return JsonResponse(
+                    {"error": "Refresh token required"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            response = JsonResponse(
+                {"message": "Logged out successfully"}, status=status.HTTP_200_OK
+            )
+
             token = RefreshToken(refresh_token)
             token.blacklist()
-            
-            response.delete_cookie(settings.ACCESS_TOKEN_COOKIE_NAME)
-            response.delete_cookie(settings.REFRESH_TOKEN_COOKIE_NAME)
+
+            delete_auth_cookies(response)
             return response
         except TokenError as e:
             print(e)
-            return JsonResponse({"error": "invalid Refresh token"}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(
+                {"error": "invalid Refresh token"}, status=status.HTTP_400_BAD_REQUEST
+            )
         except KeyError:
-            return JsonResponse({"error": "Refresh token required"}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(
+                {"error": "Refresh token required"}, status=status.HTTP_400_BAD_REQUEST
+            )
         except Exception as e:
             print(e)
-            return JsonResponse({'error': 'Unknown error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return JsonResponse(
+                {"error": "Unknown error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
