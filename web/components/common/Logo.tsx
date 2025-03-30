dd9e33d62cbf5c486f6bec3a9ad7bf55b50ae795
yujhenchen <yujhen.chen93@gmail.com>
2025-03-30 14:29:40 +0800

    feat: update logo

--- web/components/common/Logo.tsx
+++ web/components/common/Logo.tsx
@@ -2,12 +2,7 @@ export default function Logo() {
 	return (
 		<div className={"p-5 mb-2"}>
 			<a href="/">
-				<img
-					src="https://placehold.co/50x50"
-					height={64}
-					width={64}
-					alt="logo"
-				/>
+				<img src="/assets/logo.svg" height={64} width={64} alt="logo" />
 			</a>
 		</div>
 	);
