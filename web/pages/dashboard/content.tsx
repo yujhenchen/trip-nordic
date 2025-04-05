d4be0ed0539005d79c71477be37c995333d36fa3
yujhenchen <yujhen.chen93@gmail.com>
2025-04-05 13:53:22 +0800

    fix: format

--- web/pages/dashboard/content.tsx
+++ web/pages/dashboard/content.tsx
@@ -59,7 +59,9 @@ export function Content() {
 				<Sidebar className="h-full border-r">
 					<SidebarHeader>
 						<div className="flex h-14 items-center px-4">
-							<h2 className="text-lg font-semibold">My Dashboard</h2>
+							<h2 className="text-lg font-semibold">
+								My Dashboard
+							</h2>
 						</div>
 					</SidebarHeader>
 					<SidebarContent className="h-full">
@@ -84,8 +86,9 @@ export function Content() {
 						{isMobile ? <SidebarTrigger className="mr-2" /> : null}
 						<h1 className="text-xl font-semibold">
 							{
-								contentSections[activeItem as keyof typeof contentSections]
-									.title
+								contentSections[
+									activeItem as keyof typeof contentSections
+								].title
 							}
 						</h1>
 					</header>
@@ -93,14 +96,16 @@ export function Content() {
 						<div className="rounded-lg border p-6">
 							<h2 className="mb-4 text-2xl font-semibold">
 								{
-									contentSections[activeItem as keyof typeof contentSections]
-										.title
+									contentSections[
+										activeItem as keyof typeof contentSections
+									].title
 								}
 							</h2>
 							<p>
 								{
-									contentSections[activeItem as keyof typeof contentSections]
-										.content
+									contentSections[
+										activeItem as keyof typeof contentSections
+									].content
 								}
 							</p>
 						</div>
