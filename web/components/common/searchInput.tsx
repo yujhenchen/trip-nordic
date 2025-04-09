d5d6e9cd8c334cf9c3a3ca4138dcace416609c10
yujhenchen <yujhen.chen93@gmail.com>
2025-04-09 21:15:32 +0800

    fix: html attributes should be camel case

--- web/components/common/searchInput.tsx
+++ web/components/common/searchInput.tsx
@@ -13,14 +13,14 @@ export function SearchInput({ className }: Props) {
 					xmlns="http://www.w3.org/2000/svg"
 					fill="none"
 					viewBox="0 0 24 24"
-					stroke-width="2"
+					strokeWidth="2"
 					stroke="currentColor"
 					aria-hidden="true"
 					className="w-5 h-5 text-gray-500 dark:text-gray-400"
 				>
 					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
+						strokeLinecap="round"
+						strokeLinejoin="round"
 						d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
 					/>
 				</svg>
