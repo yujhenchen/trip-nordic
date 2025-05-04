46d7350b27e0be59869c9f99d5c1e2c2f7fadc6c
yujhenchen <yujhen.chen93@gmail.com>
2025-05-04 22:51:25 +0800

    feat: get filters from graph ql

--- web/pages/explore/FilterPanel.tsx
+++ web/pages/explore/FilterPanel.tsx
@@ -5,10 +5,19 @@ import {
 	FilterTitle,
 	FilterChip,
 } from "./Filter";
-import { useFilters } from "./FilterProvider";
+// import { useFilters } from "./FilterProvider";
 import { SearchInput } from "@/components/common/searchInput";
 import { HorizontalScrollArea } from "@/components/common/horizontalScrollArea";
-import { FilterKeyTitle, type FilterKeyType } from "@/types/explore";
+import type {
+	// FilterKeyTitle,
+	GQLFilterData,
+	GQLFilterResponse,
+	FilterKeyType,
+} from "@/types/explore";
+import { gql } from "graphql-request";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { graphqlClient } from "@/graphql/client";
+import { LoadingSpinner } from "@/components/ui/loading-spinner";
 
 export interface Props {
 	chipIcon: React.ReactNode;
@@ -24,20 +33,59 @@ export interface FilterPanelRow {
 	options: Array<string>;
 }
 
+const filtersQuery = gql`
+	query GetFilters {
+		filters {
+			name
+			items
+		}
+	}
+`;
+
 export function FilterPanel({
 	title,
 	chipIcon,
 	className,
-	toggleOption,
-	onReset,
+	// toggleOption,
+	// onReset,
 	onResetAll,
 }: Props) {
-	const { filters, currentFilters } = useFilters();
+	// const {
+	// 	filters,
+	// 	currentFilters,
+	// } = useFilters();
+
+	const { data, isLoading, isError } = useQuery<Array<GQLFilterData>>({
+		queryKey: ["filters"],
+		queryFn: async (): Promise<Array<GQLFilterData>> => {
+			try {
+				const result = await graphqlClient.request<GQLFilterResponse>(
+					filtersQuery,
+					{}
+				);
+				return result.filters;
+			} catch (err) {
+				return Promise.reject(err);
+			}
+		},
+		placeholderData: keepPreviousData,
+	});
+
+	if (isLoading) {
+		return <LoadingSpinner />;
+	}
+
+	if (isError) {
+		return null;
+	}
+
+	const filters = data ?? [];
+
 	return (
 		<div className={className}>
 			{title ? <FilterTitle>{title}</FilterTitle> : null}
 			<FilterContent>
-				{Object.entries(filters).map(([key, options]) => {
+				{/* {Object.entries(filters).map(([key, options]) => {
 					const filterKey = key as FilterKeyType;
 					return (
 						<div key={key} className="flex items-center space-x-3">
@@ -64,6 +112,53 @@ export function FilterPanel({
 							<Button
 								variant="default"
 								onClick={() => onReset(filterKey)}
+								className="rounded-full"
+							>
+								Reset
+							</Button>
+						</div>
+					);
+				})} */}
+				{filters.map((filter) => {
+					const options: Array<string> = JSON.parse(filter.items);
+					return (
+						<div
+							key={filter.name}
+							className="flex items-center space-x-3"
+						>
+							<FilterRowTitle className="flex-shrink-0 w-24">
+								{filter.name}
+							</FilterRowTitle>
+							<HorizontalScrollArea>
+								{options.map((option) => (
+									<FilterChip
+										key={option}
+										selected={
+											// 	Boolean(
+											// 	currentFilters[filterKey]?.includes(
+											// 		option
+											// 	)
+											// )
+											// TODO: make this to really check selected
+											false
+										}
+										value={option}
+										selectedIcon={chipIcon}
+										onClick={() =>
+											// toggleOption(filterKey, option)
+											// TODO: make this to really toggle
+											console.log(option)
+										}
+									/>
+								))}
+							</HorizontalScrollArea>
+							<Button
+								variant="default"
+								onClick={() =>
+									// onReset(filterKey)
+									// TODO: make this to really reset
+									console.log(filter.name)
+								}
 								className="rounded-full"
 							>
 								Reset
