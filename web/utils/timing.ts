af1741f751223f6b3f627a902fc74ad773fec9c8
yujhenchen <yujhen.chen93@gmail.com>
2025-05-21 20:39:31 +0800

    fix: lint

--- web/utils/timing.ts
+++ web/utils/timing.ts
@@ -1,9 +1,12 @@
-export function debounce(callback: (...args: Array<unknown>) => void, delay: number): (...args: Array<unknown>) => void {
+export function debounce(
+	callback: (...args: Array<unknown>) => void,
+	delay: number,
+): (...args: Array<unknown>) => void {
 	let timer: NodeJS.Timeout | undefined = undefined;
-	return function (...args: Array<unknown>) {
+	return (...args: Array<unknown>) => {
 		clearTimeout(timer);
 		timer = setTimeout(() => callback(...args), delay);
-	}
+	};
 }
 
-export function throttle() { }
+export function throttle() {}
