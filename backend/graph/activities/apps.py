10671c39e2093732818bc693b9c876c0783914ac
yujhenchen <yujhen.chen93@gmail.com>
2025-05-23 11:30:59 +0800

    feat: remove not need python graph repo

--- backend/graph/activities/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ActivitiesConfig(AppConfig):
-    default_auto_field = 'django_mongodb_backend.fields.ObjectIdAutoField' # 'django.db.models.BigAutoField'
-    name = 'activities'
