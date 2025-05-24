69dc69e2079e530cc0a4c8843c8635ddee66e1f8
yujhenchen <yujhen.chen93@gmail.com>
2025-05-24 15:47:18 +0800

    feat: make header menu style nicer, dynamic menu items

--- web/components/common/Header.tsx
+++ web/components/common/Header.tsx
@@ -4,11 +4,44 @@ import {
 	NavigationMenuLink,
 	NavigationMenuList,
 } from "@radix-ui/react-navigation-menu";
-import { navigationMenuTriggerStyle } from "../ui/navigation-menu";
 import { ModeToggle } from "./modeToggle";
 import { ProfileLink } from "./profileLink";
 import Logo from "./logo";
-import { defaultTrip } from "@/states/useTripsState";
+import { defaultTrip, useTrips } from "@/states/useTripsState";
+import { cn } from "@/lib/utils";
+import { cva } from "class-variance-authority";
+
+const menuItemLinkStyle = cva(
+	"relative inline-flex items-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:text-accent-foreground focus:outline-none focus:text-accent-foreground disabled:opacity-50 disabled:pointer-events-none after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-[2px] after:bg-accent-foreground after:transition-all after:duration-300 after:origin-center hover:after:left-0 hover:after:w-full"
+);
+
+function MenuItems() {
+	const trips = useTrips();
+
+	const menuItems = [
+		{
+			key: "plan",
+			text: "Plan",
+			href:
+				trips.length > 0
+					? `/plan/${trips[0].id}`
+					: `/plan/${defaultTrip.id}`,
+		},
+		{ key: "explore", text: "Explore", href: "/explore" },
+		{ key: "about", text: "About", href: "/about" },
+	];
+
+	return menuItems.map((item) => (
+		<NavigationMenuItem key={item.key}>
+			<NavigationMenuLink
+				className={cn(menuItemLinkStyle(), "bg-transparent")}
+				href={item.href}
+			>
+				{item.text}
+			</NavigationMenuLink>
+		</NavigationMenuItem>
+	));
+}
 
 export function Header() {
 	return (
@@ -17,32 +50,7 @@ export function Header() {
 			<NavigationMenu className="w-full px-6">
 				<NavigationMenuList className="flex items-center">
 					<div className="flex grow">
-						<NavigationMenuItem>
-							<NavigationMenuLink
-								className={navigationMenuTriggerStyle()}
-								href={`/plan/${defaultTrip.id}`}
-							>
-								Plan
-							</NavigationMenuLink>
-						</NavigationMenuItem>
-
-						<NavigationMenuItem>
-							<NavigationMenuLink
-								className={navigationMenuTriggerStyle()}
-								href="/explore"
-							>
-								Explore
-							</NavigationMenuLink>
-						</NavigationMenuItem>
-
-						<NavigationMenuItem>
-							<NavigationMenuLink
-								className={navigationMenuTriggerStyle()}
-								href="/about"
-							>
-								About
-							</NavigationMenuLink>
-						</NavigationMenuItem>
+						<MenuItems />
 					</div>
 
 					<div className="flex gap-6 items-center">
