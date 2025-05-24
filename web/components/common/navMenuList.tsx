3ffb8c4aa0e55949aba6f5c7a096aedd2c199142
yujhenchen <yujhen.chen93@gmail.com>
2025-05-24 19:36:59 +0800

    fix: lint

--- web/components/common/navMenuList.tsx
+++ web/components/common/navMenuList.tsx
@@ -6,10 +6,10 @@ import {
 	NavigationMenuList,
 } from "@radix-ui/react-navigation-menu";
 import { cva } from "class-variance-authority";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 const menuItemLinkStyle = cva(
-	"relative inline-flex items-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:text-accent-foreground focus:outline-none focus:text-accent-foreground disabled:opacity-50 disabled:pointer-events-none after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-[2px] after:bg-accent-foreground after:transition-all after:duration-300 after:origin-center hover:after:left-0 hover:after:w-full"
+	"relative inline-flex items-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:text-accent-foreground focus:outline-none focus:text-accent-foreground disabled:opacity-50 disabled:pointer-events-none after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-[2px] after:bg-accent-foreground after:transition-all after:duration-300 after:origin-center hover:after:left-0 hover:after:w-full",
 );
 
 type MenuItemType = {
@@ -26,9 +26,7 @@ function MenuItems() {
 			key: "plan",
 			text: "Plan",
 			href:
-				trips.length > 0
-					? `/plan/${trips[0].id}`
-					: `/plan/${defaultTrip.id}`,
+				trips.length > 0 ? `/plan/${trips[0].id}` : `/plan/${defaultTrip.id}`,
 		},
 		{ key: "explore", text: "Explore", href: "/explore" },
 		{ key: "about", text: "About", href: "/about" },
