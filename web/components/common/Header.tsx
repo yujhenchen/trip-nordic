b1a3d5e542c9640801173ada834367d0cfd470c4
yujhenchen <yujhen.chen93@gmail.com>
2025-05-24 21:30:29 +0800

    dynamic render header child components

--- web/components/common/Header.tsx
+++ web/components/common/Header.tsx
@@ -6,24 +6,34 @@ import { ModeToggle } from "./modeToggle";
 import { ProfileLink } from "./profileLink";
 import Logo from "./logo";
 import { NavMenuList } from "./navMenuList";
+import { cn } from "@/lib/utils";
 
-export function Header() {
+interface Props {
+	showLogo?: boolean;
+	showNavMenu?: boolean;
+	showModeToggle?: boolean;
+}
+
+export function Header({
+	showLogo = true,
+	showNavMenu = true,
+	showModeToggle = true,
+}: Props) {
 	return (
-		<header className="flex p-4 items-center w-full h-24">
-			<Logo />
-			<NavigationMenu className="w-full px-6">
-				<NavMenuList>
-					<div className="flex gap-6 items-center">
-						<NavigationMenuItem>
-							<ProfileLink />
-						</NavigationMenuItem>
-
-						<NavigationMenuItem>
-							<ModeToggle />
-						</NavigationMenuItem>
-					</div>
-				</NavMenuList>
-			</NavigationMenu>
+		<header className={cn("flex p-4 items-center w-full h-24")}>
+			{showLogo && <Logo />}
+			{showNavMenu && (
+				<NavigationMenu className="w-full px-6">
+					<NavMenuList>
+						<div className="flex gap-6 items-center">
+							<NavigationMenuItem>
+								<ProfileLink />
+							</NavigationMenuItem>
+						</div>
+					</NavMenuList>
+				</NavigationMenu>
+			)}
+			{showModeToggle && <ModeToggle className="px-2" />}
 		</header>
 	);
 }
