da302c41fb0b4556cdfe8da36d82a96e2c403f11
yujhenchen <yujhen.chen93@gmail.com>
2025-05-24 14:56:45 +0800

    feat: add page type and random background class to vike page context interface properties

--- web/types/vike.d.ts
+++ web/types/vike.d.ts
@@ -1,15 +1,15 @@
+import type { PageType } from "./shared";
+
 declare global {
 	namespace Vike {
 		interface PageContext {
 			// Type of pageContext.user
 			user?: string;
 			// Refine type of pageContext.Page (it's `unknown` by default)
+			pageType: PageType;
+			// Type of pageContext.randomBgClass
+			randomBgClass: string;
 			Page: () => React.JSX.Element;
 		}
 	}
 }
-
-// If you define Vike.PageContext in a .d.ts file then
-// make sure there is at least one export/import statement.
-// Tell TypeScript this file isn't an ambient module:
-export {};
