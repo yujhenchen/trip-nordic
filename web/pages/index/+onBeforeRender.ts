2bc6272233ec3446c151bbdd924952d6272fc20d
yujhenchen <yujhen.chen93@gmail.com>
2025-05-24 15:10:25 +0800

    feat: use default bg image in onBeforeRender

--- web/pages/index/+onBeforeRender.ts
+++ web/pages/index/+onBeforeRender.ts
@@ -5,8 +5,10 @@ import { COUNTRY_BG_CLASSES } from "@/constants";
 export function onBeforeRender() {
 	const pageType = "home";
 
-	const randomIndex = Math.floor(Math.random() * COUNTRY_BG_CLASSES.length);
-	const randomBgClass = COUNTRY_BG_CLASSES[randomIndex];
+	// TODO: use this once setup CDN
+	// const randomIndex = Math.floor(Math.random() * COUNTRY_BG_CLASSES.length);
+	// const randomBgClass = COUNTRY_BG_CLASSES[randomIndex];
+	const randomBgClass = "bg-[url('/assets/fi.jpg')]";
 
 	return {
 		pageContext: {
