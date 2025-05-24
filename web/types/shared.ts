261521c0ebaa1779e3114bc44885f07f0768df1a
yujhenchen <yujhen.chen93@gmail.com>
2025-05-24 14:54:43 +0800

    feat: add page key object and type

--- web/types/shared.ts
+++ web/types/shared.ts
@@ -2,3 +2,15 @@ export interface AppDateRange {
 	from: Date;
 	to: Date;
 }
+
+export const PAGE_KEYS = {
+	ABOUT: "about",
+	DASHBOARD: "dashboard",
+	EXPLORE: "explore",
+	HOME: "home",
+	LOGIN: "login",
+	SIGNUP: "signup",
+	PLAN: "plan",
+} as const;
+
+export type PageType = (typeof PAGE_KEYS)[keyof typeof PAGE_KEYS];
