35f3b0924993477a6ef570e71c6e5ae4b608629e
yujhenchen <yujhen.chen93@gmail.com>
2025-05-25 12:05:59 +0800

    feat: add page shared before render

---
+++ web/pages/+onBeforeRender.ts
@@ -0,0 +1,31 @@
+// /pages/some-page/+onBeforeRender.js
+
+import { COUNTRY_BG_CLASSES } from "@/constants";
+import { BgImgClassStore } from "@/globalStore";
+import type { PageContext } from "vike/types";
+
+export function onBeforeRender(pageContext: PageContext) {
+	let currentPath = pageContext.urlPathname;
+	const bgClassStore = BgImgClassStore.getInstance();
+
+	// process plan
+	if (currentPath.includes("/plan")) {
+		currentPath = "/plan";
+	}
+
+	// get new image when at home
+	if (currentPath === "/") {
+		const randomBgClass =
+			COUNTRY_BG_CLASSES[
+				Math.floor(Math.random() * COUNTRY_BG_CLASSES.length)
+			] ?? "";
+		bgClassStore.setBgImgClass(randomBgClass);
+	}
+
+	return {
+		pageContext: {
+			pageType: currentPath,
+			bgImgClass: bgClassStore.getBgImgClass(),
+		},
+	};
+}
