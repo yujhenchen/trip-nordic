1f8e2b46f53c3e1e539337646160850bc70c7a1e
yujhenchen <yujhen.chen93@gmail.com>
2025-05-25 15:08:24 +0800

    feat: dynamic footer components

--- web/components/common/Footer.tsx
+++ web/components/common/Footer.tsx
@@ -2,25 +2,45 @@ import { NavigationMenu } from "@radix-ui/react-navigation-menu";
 import { CopyRight } from "./copyRight";
 import { NavMenuList } from "./navMenuList";
 import { ModeToggle } from "./modeToggle";
+import { ProfileMenuItem } from "./profile";
+import { cn } from "@/lib/utils";
 
 export function Footer({
-	showHome = false,
-	showModeToggle = false,
+	showHome = true,
+	showNavMenu = true,
+	showModeToggle = true,
+	showProfile = true,
 }: {
 	showHome?: boolean;
+	showNavMenu?: boolean;
 	showModeToggle?: boolean;
+	showProfile?: boolean;
 }) {
 	return (
-		<footer className="bg-gray-200 rounded-t-[2rem] shadow dark:bg-gray-800 w-full">
-			<div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-				<CopyRight />
+		<footer
+			className={cn(
+				"bg-gray-200 rounded-t-3xl shadow dark:bg-gray-800 w-full",
+				"mx-auto max-w-screen-xl px-8 py-4 md:flex md:items-center md:justify-between",
+			)}
+		>
+			<CopyRight />
 
+			{showNavMenu && (
 				<NavigationMenu>
 					<NavMenuList showHome={showHome} />
 				</NavigationMenu>
+			)}
 
-				{showModeToggle && <ModeToggle />}
-			</div>
+			{(showProfile || showModeToggle) && (
+				<div className="flex items-center space-x-4">
+					{showProfile && (
+						<NavigationMenu>
+							<ProfileMenuItem />
+						</NavigationMenu>
+					)}
+					{showModeToggle && <ModeToggle />}
+				</div>
+			)}
 		</footer>
 	);
 }
