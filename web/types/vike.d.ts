0a00fdfb297b63a17fd9051affcdd0cb0b939ffe
yujhenchen <yujhen.chen93@gmail.com>
2025-05-25 15:05:26 +0800

    fix: custom page context cannot find properties

--- web/types/vike.d.ts
+++ web/types/vike.d.ts
@@ -1,15 +1,15 @@
-import type { RoutePath } from "./shared";
-
 declare global {
 	namespace Vike {
 		interface PageContext {
 			// Type of pageContext.user
 			user?: string;
 			// Refine type of pageContext.Page (it's `unknown` by default)
-			routePath: RoutePath;
+			routePath: string;
 			// Type of pageContext.bgImgClass
 			bgImgClass: string;
 			Page: () => React.JSX.Element;
 		}
 	}
 }
+
+export { }
