10c859e1db7567d4c26798c6d39bf9fc159f1774
yujhenchen <yujhen.chen93@gmail.com>
2025-06-13 18:15:10 +0800

    feat: cloudinary img ids store

--- web/globalStore.ts
+++ web/globalStore.ts
@@ -1,3 +1,5 @@
+import { getAssetsPublicIds } from "./cloudinary";
+
 export class BgImgUrlStore {
 	private bgImgUrl = "";
 	private static instance: BgImgUrlStore;
@@ -17,5 +19,36 @@ export class BgImgUrlStore {
 
 	public setBgImgUrl(imgUrl: string): void {
 		this.bgImgUrl = imgUrl;
+	}
+}
+
+const imgPrefix: string = process.env.CLOUDINARY_IMAGE_PREFIX ?? "img_prefix";
+
+export class ImageIdsStore {
+	private static instance: ImageIdsStore;
+	private imgIds: Array<string> = [];
+
+	private constructor() {}
+
+	private async init(): Promise<void> {
+		try {
+			const imgIds = await getAssetsPublicIds({ prefix: imgPrefix });
+			this.imgIds = imgIds;
+		} catch (error) {
+			console.error(error);
+			this.imgIds = [];
+		}
+	}
+
+	public static async getInstance(): Promise<ImageIdsStore> {
+		if (!ImageIdsStore.instance) {
+			ImageIdsStore.instance = new ImageIdsStore();
+			await ImageIdsStore.instance.init();
+		}
+		return ImageIdsStore.instance;
+	}
+
+	public getImgIds(): Array<string> {
+		return this.imgIds;
 	}
 }
