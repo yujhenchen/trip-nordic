d1b8bb7f170a91a1c9f81bced6866754069058fc
yujhenchen <yujhen.chen93@gmail.com>
2025-06-13 16:12:10 +0800

    feat: set bg img using cloudinary

--- web/pages/+onBeforeRender.ts
+++ web/pages/+onBeforeRender.ts
@@ -1,12 +1,30 @@
 // /pages/some-page/+onBeforeRender.js
 
-import { COUNTRY_BG_CLASSES } from "@/constants";
 import { BgImgClassStore } from "@/globalStore";
 import type { PageContext } from "vike/types";
+
+import { Cloudinary } from "@cloudinary/url-gen";
+import { auto } from "@cloudinary/url-gen/actions/resize";
+import { autoGravity } from "@cloudinary/url-gen/qualifiers/gravity";
+
+const cloudName: string = process.env.CLOUDINARY_CLOUD_NAME ?? "test";
 
 export function onBeforeRender(pageContext: PageContext) {
 	let currentPath = pageContext.urlPathname;
 	const bgClassStore = BgImgClassStore.getInstance();
+
+	// TODO: use API key to secure
+	const cld = new Cloudinary({ cloud: { cloudName } });
+
+	// Use this sample image or upload your own via the Media Explorer
+	// TODO: random get images from Cloudinary instance
+	const img = cld
+		.image("se-3_slai5b")
+		.format("auto") // Optimize delivery by resizing and applying auto-format and auto-quality
+		.quality("auto")
+		.resize(auto().gravity(autoGravity())); // Transform the image: auto-crop to square aspect_ratio
+
+	const imgUrl = img.toURL();
 
 	// process plan
 	if (currentPath.includes("/plan")) {
@@ -15,11 +33,8 @@ export function onBeforeRender(pageContext: PageContext) {
 
 	// get new image when at home
 	if (currentPath === "/") {
-		const randomBgClass =
-			COUNTRY_BG_CLASSES[
-				Math.floor(Math.random() * COUNTRY_BG_CLASSES.length)
-			] ?? "";
-		bgClassStore.setBgImgClass(randomBgClass);
+		const bgClass = imgUrl;
+		bgClassStore.setBgImgClass(bgClass);
 	}
 
 	return {
