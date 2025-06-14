b01a8f3921941f741ed10020a6aa26d16c8701d2
yujhenchen <yujhen.chen93@gmail.com>
2025-06-14 16:22:26 +0800

    feat: before render init bg img store, add fallback bg img

--- web/pages/+onBeforeRender.ts
+++ web/pages/+onBeforeRender.ts
@@ -1,51 +1,40 @@
 // /pages/some-page/+onBeforeRender.js
 
-import { BgImgUrlStore, ImageIdsStore } from "@/globalStore";
+import { BgImgStore } from "@/globalStore";
 import type { PageContext } from "vike/types";
 
-import { Cloudinary } from "@cloudinary/url-gen";
-import { auto, scale } from "@cloudinary/url-gen/actions/resize";
-import { autoGravity } from "@cloudinary/url-gen/qualifiers/gravity";
-
 const cloudName: string = process.env.CLOUDINARY_CLOUD_NAME ?? "test";
+const imgPrefix: string = process.env.CLOUDINARY_IMAGE_PREFIX ?? "img_prefix";
+
+const fallbackImgUrl = "/assets/fi.jpg";
 
 export async function onBeforeRender(pageContext: PageContext) {
 	let currentPath = pageContext.urlPathname;
-	const bgImgUrlStore = BgImgUrlStore.getInstance();
 
-	const imgIdsStore = await ImageIdsStore.getInstance();
-	const imgIds = imgIdsStore.getImgIds();
-	const randomIndex = Math.floor(Math.random() * imgIds.length);
+	const storeInstance = await BgImgStore.getInstance(cloudName, imgPrefix);
+	const isHome = currentPath === "/";
 
-	const cld = new Cloudinary({ cloud: { cloudName } });
-
-	// process plan
 	if (currentPath.includes("/plan")) {
 		currentPath = "/plan";
 	}
 
-	let imgId = bgImgUrlStore.getBgImgId();
-
-	if (currentPath === "/") {
-		imgId = imgIds[randomIndex];
-		bgImgUrlStore.setBgImgId(imgId);
+	let imgId = "";
+	if (isHome) {
+		const imgIdCount = storeInstance.getImgIds().length;
+		const randomIdx = Math.floor(Math.random() * imgIdCount);
+		imgId = storeInstance.getImgIds()[randomIdx];
+		storeInstance.setCurrentImgId(imgId);
 	}
 
-	let img = cld
-		.image(imgId)
-		.format("auto")
-		.resize(auto().gravity(autoGravity()));
-	if (currentPath !== "/") {
-		img = img.resize(scale().width(20));
-	}
-
-	const imgUrl = img.toURL();
-	bgImgUrlStore.setBgImgUrl(imgUrl);
+	imgId = storeInstance.getCurrentImgId();
+	const imgUrlItem = storeInstance.getItemFromImgUrlMap(imgId);
 
 	return {
 		pageContext: {
 			routePath: currentPath,
-			bgImgUrl: bgImgUrlStore.getBgImgUrl(),
+			bgImgUrl: isHome
+				? (imgUrlItem?.default ?? fallbackImgUrl)
+				: (imgUrlItem?.small ?? fallbackImgUrl),
 		},
 	};
 }
