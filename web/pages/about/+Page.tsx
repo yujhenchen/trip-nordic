1623231d0261105720fefcf253cd37d92321f21e
yujhenchen <yujhen.chen93@gmail.com>
2025-06-15 18:10:20 +0800

    feat: wrap about into page container

--- web/pages/about/+Page.tsx
+++ web/pages/about/+Page.tsx
@@ -1,5 +1,13 @@
+import { ContentContainer } from "@/components/common/contentContainer";
 import { PageContainer } from "@/components/common/pageContainer";
+import { H2 } from "@/components/typography/h2";
 
 export default function AboutPage() {
-	return <PageContainer>about</PageContainer>;
+	return (
+		<PageContainer>
+			<ContentContainer>
+				<H2>About</H2>
+			</ContentContainer>
+		</PageContainer>
+	);
 }
