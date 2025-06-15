43a1b2854bed010275e09cb696be2237c8f65ee7
yujhenchen <yujhen.chen93@gmail.com>
2025-06-15 18:05:55 +0800

    feat: allow toggle full height

--- web/components/ui/scroll-area.tsx
+++ web/components/ui/scroll-area.tsx
@@ -19,20 +19,43 @@ import { cn } from "@/lib/utils";
 // 		<ScrollAreaPrimitive.Corner />
 // 	</ScrollAreaPrimitive.Root>
 // ));
+
+type ScrollAreaProps = React.ComponentPropsWithoutRef<
+	typeof ScrollAreaPrimitive.Root
+> & {
+	fullHeight?: boolean;
+};
 const ScrollArea = React.forwardRef<
 	React.ElementRef<typeof ScrollAreaPrimitive.Root>,
-	React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
->(({ className, children, ...props }, ref) => {
+	ScrollAreaProps
+>(({ className, children, fullHeight = false, ...props }, ref) => {
 	const innerRef = React.useRef<HTMLDivElement>(null);
 	React.useEffect(() => {
 		const firstChild = innerRef.current?.querySelector(
-			"[data-radix-scroll-area-viewport]",
+			"[data-radix-scroll-area-viewport]"
 		)?.firstElementChild as HTMLElement | null;
 
 		if (firstChild) {
 			firstChild.style.display = "block";
 		}
 	}, []);
+	// NOTE: custom changes to make viewport first div full height
+	const viewportRef = React.useRef<HTMLDivElement | null>(null);
+
+	React.useEffect(() => {
+		if (!fullHeight) {
+			return;
+		}
+		if (viewportRef.current) {
+			const firstChild = viewportRef.current
+				.firstElementChild as HTMLElement | null;
+
+			if (firstChild) {
+				firstChild.style.height = "100%";
+			}
+		}
+	}, [fullHeight]);
+
 	return (
 		<ScrollAreaPrimitive.Root
 			ref={(node) => {
@@ -40,13 +63,18 @@ const ScrollArea = React.forwardRef<
 				if (typeof ref === "function") {
 					ref(node);
 				} else if (ref) {
-					(ref as React.MutableRefObject<HTMLDivElement | null>).current = node;
+					(
+						ref as React.MutableRefObject<HTMLDivElement | null>
+					).current = node;
 				}
 			}}
 			className={cn("relative overflow-hidden", className)}
 			{...props}
 		>
-			<ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
+			<ScrollAreaPrimitive.Viewport
+				ref={viewportRef}
+				className="h-full w-full rounded-[inherit]"
+			>
 				{children}
 			</ScrollAreaPrimitive.Viewport>
 			<ScrollBar />
@@ -58,7 +86,9 @@ ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName;
 
 const ScrollBar = React.forwardRef<
 	React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>,
-	React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
+	React.ComponentPropsWithoutRef<
+		typeof ScrollAreaPrimitive.ScrollAreaScrollbar
+	>
 >(({ className, orientation = "vertical", ...props }, ref) => (
 	<ScrollAreaPrimitive.ScrollAreaScrollbar
 		ref={ref}
@@ -69,7 +99,7 @@ const ScrollBar = React.forwardRef<
 				"h-full w-2.5 border-l border-l-transparent p-[1px]",
 			orientation === "horizontal" &&
 				"h-2.5 flex-col border-t border-t-transparent p-[1px]",
-			className,
+			className
 		)}
 		{...props}
 	>
